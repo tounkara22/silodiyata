d5bf6e32e0228ea0f4827d5348dbf2d1ff79bae5
tounkara22 <tounkara@stanford.edu>
2022-03-21 17:41:31 -0400

    Complete MVP of the home page. Header/Footer, Hero, Details

--- src/language/en.js
+++ src/language/en.js
@@ -8,7 +8,37 @@ const i18n = {
     footer: {},
   },
   home: {
-    hero: {},
+    hero: {
+      title: "Looking for a smooth ride to and from the Airport?",
+      subtitle:
+        "In just few clicks, book a ride and we ensure you have a relaxing ride to your destination",
+      description:
+        "If you want family and friends to welcome you, we can make that happen too.",
+      bookButton: "Book now",
+      contactButton: "Contact us",
+    },
+    details: {
+      detail1: {
+        title: "We'll be on time".toUpperCase(),
+        subtitle:
+          "We track your flight details in real time in order to make sure we are ready to pick you up or drop you off to the airport without delay.",
+        details:
+          "Make sure to correctly let us know your flight details upon booking",
+      },
+      detail2: {
+        title: "Enjoy world class comfort".toUpperCase(),
+        subtitle:
+          "Do you seek to rest before reaching your destination? Do you want to hangout with your close ones? However you choose to ride, we make sure to do just that.",
+        details:
+          "We have Wi-Fi, snacks and Music streaming onboard. Just let us know!",
+      },
+      detail3: {
+        title: "Always available to chat".toUpperCase(),
+        subtitle:
+          "We ensure to be available before, during and after your ride. Our riders are vetted and should you have any need, we are more than happy to help.",
+        details: "",
+      },
+    },
   },
 };
 
